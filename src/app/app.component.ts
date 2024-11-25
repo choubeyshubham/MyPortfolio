e3edcc571bd44a61e5feb37e5d1a1ad9083cd06e
Shubham Choubey <shubhamchoubey96@gmail.com>
2024-11-25 23:12:33 +0530

    04 commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HeaderComponent} from "./components/header/header.component";
+import {ProfileComponent} from "./components/profile/profile.component";
+import {AboutComponent} from "./components/about/about.component";
+import {ExperienceComponent} from "./components/experience/experience.component";
+import {ProjectsComponent} from "./components/projects/projects.component";
+import {ContactComponent} from "./components/contact/contact.component";
+import {FooterComponent} from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, ProfileComponent, AboutComponent, ExperienceComponent, ProjectsComponent, ContactComponent, FooterComponent],
+  templateUrl: './app.component.html'
 })
+
 export class AppComponent {
   title = 'MyProtfolio';
 }
